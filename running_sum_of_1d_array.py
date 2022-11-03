38ebc81095e44c883ee32566df6081d9249da6cf
boluwatifeh <tifeasypeasy@gmail.com>
2022-11-03 23:39:59 +0100

    edit url to the question on LC

--- running_sum_of_1d_array.py
+++ running_sum_of_1d_array.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/running-sum-of-1d-array/submissions/
+# https://leetcode.com/problems/running-sum-of-1d-array
 
 class Solution:
     def runningSum(self, nums: list[int]) -> list[int]:
