b01df658be75ba76cef335dc0b60491ad99feb78
boluwatifeh <tifeasypeasy@gmail.com>
2022-11-04 18:26:54 +0100

    solved the problem using list comprehension one liner

--- running_sum_of_1d_array.py
+++ running_sum_of_1d_array.py
@@ -10,3 +10,11 @@ class Solution:
 # test input [1,2,3,4] -> output [1, 3, 7, 10] i.e [1, 1+2, 1+2+3, 1+2+3+4]
 test = Solution()
 print(test.runningSum([1,2,3,4]))
+
+
+#using list comprehension
+def running_sum(nums: list[int]) -> list[int]:
+    return [sum(nums[:i+1]) for i in range(len(nums))] 
+#test input [2,34,6,9,7] -> expected output [2, 36, 42, 51, 58]
+verify = running_sum([2,34,6,9,7])
+print(verify)
