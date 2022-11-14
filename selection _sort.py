f43f2687f8627b0da36e079c88d674954fb42795
boluwatifeh <tifeasypeasy@gmail.com>
2022-11-14 22:31:18 +0100

    implement selection sort algorithm

---
+++ selection _sort.py
@@ -0,0 +1,15 @@
+# Implment selection sort algorithm
+def selectionSort(unsorted_list):
+    for i in range(len(unsorted_list)):
+        min_val = i
+        for j in range(i+1, len(unsorted_list)):
+            if unsorted_list[j] < unsorted_list[i]:
+                min_val = j
+        temp = unsorted_list[i]
+        unsorted_list[i] = unsorted_list[min_val]
+        unsorted_list[min_val] = temp
+    
+
+nums = [5,3,8,6,7,2]
+selectionSort(nums)
+print(nums)
