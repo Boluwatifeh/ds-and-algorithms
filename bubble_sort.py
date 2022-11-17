7a096b329d595ab9d367975f5be9af6adb43cf6f
boluwatifeh <tifeasypeasy@gmail.com>
2022-11-17 22:46:47 +0100

    remove debuging print statements

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,12 +1,9 @@
 def bubble_sort(nums:list[int]):
     for j in range(len(nums)):
         for i in range(len(nums)-1):
-            print(nums[i], nums[i+1])
             if nums[i] > nums[i+1]:
                 nums[i], nums[i+1] = nums[i+1], nums[i]
-            print(nums[i], nums[i+1])
-            
-    return nums
+    return (f"sorted list -> {nums}")
             
 
 
