d26b3654245dc6dd9b432524e778315c67fad77e
boluwatifeh <tifeasypeasy@gmail.com>
2022-11-23 23:56:32 +0100

    add linkedlist class

--- linked_list.py
+++ linked_list.py
@@ -6,3 +6,11 @@ class Node:
         self.data = data
     def __repr__(self) -> str:
         return f"data in the node -> {self.data}"
+
+class Linkedlist:
+
+    def __init__(self):
+        self.head = None 
+    
+    def is_empty(self):
+        return self.head == None
